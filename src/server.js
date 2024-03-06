67bf91f4349e7b7716839325958390d1e217a2fb
Milind <milindmishra11@gmail.com>
2024-03-07 03:01:11 +0530

    feat: "made changes to db code"

--- src/server.js
+++ src/server.js
@@ -8,33 +8,9 @@ const app = express();
 const PORT = 5000;
 app.use(cors());
 
-// Connect to MongoDB (replace 'YOUR_MONGODB_URI' with your actual MongoDB URI)
-connect(
-  "mongodb+srv://user123:qCLWtaO41eNHnlwK@cluster0.yn6gak9.mongodb.net/",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
-
-// Define a Mongoose schema for cards
-const cardSchema = new Schema({
-  name: String,
-  role: String,
-  interests: String,
-  githubUsername: String,
-  socialMedia: [{ name: String, link: String }],
-});
-
-// Create a Mongoose model for cards
-const Card = model("Card", cardSchema);
-
-// Middleware to parse JSON body
 app.use(json());
 
-// Route handler to add a new card
+
 app.post("/api/cards", async (req, res) => {
   try {
     const newCard = new Card(req.body);
@@ -45,7 +21,7 @@ app.post("/api/cards", async (req, res) => {
   }
 });
 
-// Route handler to fetch all cards
+
 app.get("/api/cards", async (req, res) => {
   try {
     const cards = await Card.find();
@@ -55,7 +31,7 @@ app.get("/api/cards", async (req, res) => {
   }
 });
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
