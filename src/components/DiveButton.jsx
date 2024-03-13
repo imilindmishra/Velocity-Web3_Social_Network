2c883731cf6f89e4cd84e601af23108fa7217a87
Milind <milindmishra11@gmail.com>
2024-03-13 16:47:03 +0530

    pushed

---
+++ src/components/DiveButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function DiveButton() {
+    const navigate = useNavigate();
+
+    const handleLandingClick = () => {
+        navigate('/bounties');
+    }   
+
+    return (
+        <div className="font-serif flex justify-center">
+            <button
+                className="bg-yellow-800 text-white mt-4 px-12 py-2 pt-3 pb-3 rounded-md text-x1 font-medium shadow-md hover:shadow-xl transition duration-300"
+                onClick={handleLandingClick}
+            >
+                Dive In →
+            </button>        
+        </div>
+    );
+}
+
+export default DiveButton;
--- src/home.jsx
+++ src/home.jsx
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar'
 import BlueCard from './components/BlueCard'
 import LandingButton from './components/LandingButton'
 import { useEffect } from 'react'
+import Dive from './components/DiveButton'
 
 
 
@@ -17,18 +18,32 @@ const Home = () => {
     }, []);
 
     return (
-    <>
-      <div className="bg-orange-100 min-h-screen">
-        <div className="relative z-10 flex flex-col items-center">
-          <h1 className="text-5xl md:text-5xl lg:text-5xl font-serif text-center leading-tight pt-28 mb-0">
-            Mint Your Identity<br />
-            Forge Your Community
-          </h1>
+    <div className="flex w-full min-h-screen">
+            <div className='w-1/2'>
+                <div className="bg-orange-100 min-h-screen">
+                    <div className="relative z-10 flex flex-col items-center">
+                        <h1 className="text-5xl md:text-5xl lg:text-5xl font-serif text-center leading-tight pt-28 mb-0">
+                            Mint Your Identity<br />
+                            Forge Your Community
+                        </h1>
+                    </div>
+                    <LandingButton className="mb-24 mt-0"/>
+                    <BlueCard />
+                </div>
+            </div>
+            <div className='w-1/2'>
+                <div className="bg-orange-100 min-h-screen">
+                    <div className="relative z-10 flex flex-col items-center">
+                        <h1 className="text-5xl md:text-5xl lg:text-5xl font-serif text-center leading-tight pt-28 mb-0">
+                            Unlock the Future,<br />
+                            One Bounty at a Time.
+                        </h1>
+                    </div>
+                    <Dive className="mb-24 mt-0"/>
+                    <BlueCard />
+                </div>
+            </div>
         </div>
-        <LandingButton className="mb-24 mt-0"/>
-        <BlueCard />
-      </div>
-    </>
   )
 }
 
